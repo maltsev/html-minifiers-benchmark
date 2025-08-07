a075794c882d71fa3cd082b766fc4d81c80259ce
Kirill Maltsev <maltsevkirill@gmail.com>
2025-08-07 20:41:47 +0300

    Upgrade to eslint@9

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                fetch: 'readonly'
+            }
+        },
+        rules: {
+            'indent': ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always']
+        }
+    }
+];
