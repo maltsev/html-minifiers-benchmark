081ebb94d076341c469c6e6a23c9f0d44be51bb5
Kirill Maltsev <maltsevkirill@gmail.com>
2025-09-04 15:12:50 +0300

    Fix bug with invalid preset for htmlnano
    It should be `htmlnano.presets.safe`. But we can also just remove it.
    (`safe` is default preset)

--- minifiers/htmlnano.js
+++ minifiers/htmlnano.js
@@ -2,18 +2,13 @@ import htmlnano from 'htmlnano';
 import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
-const safePreset = htmlnano.presets.safePreset;
 
 export default async function (html) {
-    const result = await htmlnano.process(
-        html,
-        {
-            collapseWhitespace: 'aggressive',
-            removeAttributeQuotes: true,
-            removeRedundantAttributes: true,
-        },
-        safePreset
-    );
+    const result = await htmlnano.process(html, {
+        collapseWhitespace: 'aggressive',
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+    });
     return result.html;
 }
 
