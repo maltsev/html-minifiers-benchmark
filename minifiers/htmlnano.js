5c80c919a2b2fc7eb13b92d28472ca108e7e771a
Kirill Maltsev <maltsevkirill@gmail.com>
2026-02-04 11:41:01 +0200

    Fix lint errors

--- minifiers/htmlnano.js
+++ minifiers/htmlnano.js
@@ -7,7 +7,7 @@ export default async function (html) {
     const result = await htmlnano.process(html, {
         ...presets.max,
         removeUnusedCss: {
-            tool: 'purgeCSS',  // uncss fails, so use purgeCSS
+            tool: 'purgeCSS', // uncss fails, so use purgeCSS
         },
     });
     return result.html;
